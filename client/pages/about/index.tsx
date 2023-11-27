ffc6f91fd4f65f43cc6f5973706cdc15799997dc
Aleksander Bondar <86482559+AleksanderBondar@users.noreply.github.com>
2023-11-27 01:04:28 +0100

    :sparkles:

--- client/pages/about/index.tsx
+++ client/pages/about/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { AboutCanvas } from '../../components/aboutCanvas';
 import { ThemeSwitcher } from '../../components/ThemeSwitcher';
 
-HTMLCanvasElement;
 function About() {
     return (
         <>
--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import Router from 'express-promise-router';
-import { API } from './api/index.js';
 import { ROUTES } from './routes/index.js';
 import { IO } from './api/io.js';
-
+import fs from 'fs';
 const router = Router({ strict: true });
 
 ROUTES(router);
-API(router);
-
+router.get(`/`, async (__, res, _) => {
+    let html = fs.readFileSync('./client/index.html', 'utf-8');
+    res.send(html);
+});
+router.get(`/about`, async (__, res, _) => {
+    let html = fs.readFileSync('./client/index.html', 'utf-8');
+    res.send(html);
+});
 router.use('*', (_, res) => {
     res.status(404).send({ message: 'Not Found' });
 });
